b6548356829c6c4884c0144670335f304cd0b0f6
Shota Togawa <s.togawa0112@gmail.com>
2019-04-03 15:11:51 -0700

    made front page

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className="tc">
+            <div className="white f3">
+                {'Someone, your current rank is...'}
+            </div>
+            <div className="white f2"> 
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank
